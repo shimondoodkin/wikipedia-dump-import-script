ba93a1ed75ba28d692f48332622f9fab508952d2
Shimon Doodkin <helpmepro1@gmail.com>
2016-03-17 13:01:14 +0200

    Create example-result.sh

---
+++ example-result.sh
@@ -0,0 +1,126 @@
+# latest url: http://dumps.wikimedia.your.org/enwiki/20160305/ 
+# selected url: http://dumps.wikimedia.your.org/enwiki/20160204/ 
+
+
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-change_tag.sql.gz  ## 97.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-geo_tags.sql.gz  ## 27.5 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-iwlinks.sql.gz  ## 109.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-redirect.sql.gz  ## 99.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-protected_titles.sql.gz  ## 1.0 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-page_props.sql.gz  ## 212.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-page_restrictions.sql.gz  ## 765 KB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-page.sql.gz  ## 1.3 GB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-category.sql.gz  ## 31.5 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-user_groups.sql.gz  ## 75 KB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-langlinks.sql.gz  ## 248.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-externallinks.sql.gz  ## 2.2 GB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-templatelinks.sql.gz  ## 1.8 GB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-imagelinks.sql.gz  ## 350.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-categorylinks.sql.gz  ## 1.6 GB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pagelinks.sql.gz  ## 4.7 GB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-image.sql.gz  ## 122.2 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-site_stats.sql.gz  ## 817 bytes
+
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles1.xml-p000000010p000030302.bz2  ## 147.9 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles2.xml-p000030304p000088444.bz2  ## 173.5 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles3.xml-p000088445p000200507.bz2  ## 222.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles4.xml-p000200511p000352689.bz2  ## 229.0 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles5.xml-p000352690p000565312.bz2  ## 251.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles6.xml-p000565314p000892912.bz2  ## 271.8 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles7.xml-p000892914p001268691.bz2  ## 282.2 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles8.xml-p001268693p001791079.bz2  ## 309.0 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles9.xml-p001791081p002336422.bz2  ## 300.8 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles10.xml-p002336425p003046511.bz2  ## 315.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles11.xml-p003046513p003926861.bz2  ## 336.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles12.xml-p003926864p005040435.bz2  ## 350.2 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles13.xml-p005040438p006197593.bz2  ## 345.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles14.xml-p006197599p007744799.bz2  ## 359.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles15.xml-p007744803p009518046.bz2  ## 389.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles16.xml-p009518059p011539266.bz2  ## 383.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles17.xml-p011539268p013693066.bz2  ## 467.2 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles18.xml-p013693075p016120541.bz2  ## 493.9 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles19.xml-p016120548p018754723.bz2  ## 484.2 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles20.xml-p018754736p021222156.bz2  ## 474.5 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles21.xml-p021222161p023927980.bz2  ## 567.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles22.xml-p023927984p026823658.bz2  ## 602.9 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles23.xml-p026823661p030503448.bz2  ## 686.3 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles24.xml-p030503454p033952815.bz2  ## 753.5 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles25.xml-p033952817p038067198.bz2  ## 814.4 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles26.xml-p038067204p042663461.bz2  ## 929.6 MB
+wget http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles27.xml-p042663464p049318148.bz2  ## 1.1 GB
+
+
+
+
+date > import-timing.txt
+for table in \
+      change_tag  geo_tags  iwlinks  redirect  protected_titles  page_props  page_restrictions  page  category  user_groups  langlinks  externallinks  templatelinks  imagelinks  categorylinks  pagelinks  image  site_stats ; \
+  do
+    echo "TRUNCATE TABLE $table ; " | mysql -B enwiki
+done
+
+for f in  \
+    enwiki-20160204-change_tag.sql.gz  enwiki-20160204-geo_tags.sql.gz  enwiki-20160204-iwlinks.sql.gz  enwiki-20160204-redirect.sql.gz  enwiki-20160204-protected_titles.sql.gz  enwiki-20160204-page_props.sql.gz  enwiki-20160204-page_restrictions.sql.gz  enwiki-20160204-page.sql.gz  enwiki-20160204-category.sql.gz  enwiki-20160204-user_groups.sql.gz  enwiki-20160204-langlinks.sql.gz  enwiki-20160204-externallinks.sql.gz  enwiki-20160204-templatelinks.sql.gz  enwiki-20160204-imagelinks.sql.gz  enwiki-20160204-categorylinks.sql.gz  enwiki-20160204-pagelinks.sql.gz  enwiki-20160204-image.sql.gz  enwiki-20160204-site_stats.sql.gz ; \
+  do
+    ( \
+      LINE=$(zcat $f|head -n 200|awk '/-- Dumping data for table/ {print FNR}') #SKIP CRATE TABLES, also see "|tail -n +$LINE" below
+      echo "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;" ; \
+      pv $f | pigz -dc  |tail -n +$LINE;  \
+      echo "SET autocommit=1; SET unique_checks=1; SET foreign_key_checks=1;" \
+    ) | mysql -B enwiki
+done
+date >> import-timing.txt
+
+
+
+##stright from web version
+
+date > import-timing.txt
+for table in \
+      change_tag  geo_tags  iwlinks  redirect  protected_titles  page_props  page_restrictions  page  category  user_groups  langlinks  externallinks  templatelinks  imagelinks  categorylinks  pagelinks  image  site_stats ; \
+  do
+    echo "TRUNCATE TABLE $table ; " | mysql -B enwiki
+done
+
+for f in  \
+    enwiki-20160204-change_tag.sql.gz  enwiki-20160204-geo_tags.sql.gz  enwiki-20160204-iwlinks.sql.gz  enwiki-20160204-redirect.sql.gz  enwiki-20160204-protected_titles.sql.gz  enwiki-20160204-page_props.sql.gz  enwiki-20160204-page_restrictions.sql.gz  enwiki-20160204-page.sql.gz  enwiki-20160204-category.sql.gz  enwiki-20160204-user_groups.sql.gz  enwiki-20160204-langlinks.sql.gz  enwiki-20160204-externallinks.sql.gz  enwiki-20160204-templatelinks.sql.gz  enwiki-20160204-imagelinks.sql.gz  enwiki-20160204-categorylinks.sql.gz  enwiki-20160204-pagelinks.sql.gz  enwiki-20160204-image.sql.gz  enwiki-20160204-site_stats.sql.gz ; \
+  do
+    ( \
+      LINE=$(wget http://dumps.wikimedia.your.org/enwiki/20160204/$f -O -|gunzip|head -n 200|awk '/-- Dumping data for table/ {print FNR}') #SKIP CRATE TABLES, also see "|tail -n +$LINE" below
+	  FSIZE=$(wget http://dumps.wikimedia.your.org/enwiki/20160204/$f --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}')
+      echo "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;" ; \
+      wget http://dumps.wikimedia.your.org/enwiki/20160204/$f --show-progress -O - | pigz -dc  |tail -n +$LINE;  \
+      echo "SET autocommit=1; SET unique_checks=1; SET foreign_key_checks=1;" \
+    ) | mysql -B enwiki
+done
+date >> import-timing.txt
+
+
+pv enwiki-20160204-pages-articles1.xml-p000000010p000030302.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles2.xml-p000030304p000088444.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles3.xml-p000088445p000200507.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles4.xml-p000200511p000352689.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles5.xml-p000352690p000565312.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles6.xml-p000565314p000892912.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles7.xml-p000892914p001268691.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles8.xml-p001268693p001791079.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles9.xml-p001791081p002336422.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles10.xml-p002336425p003046511.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles11.xml-p003046513p003926861.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles12.xml-p003926864p005040435.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles13.xml-p005040438p006197593.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles14.xml-p006197599p007744799.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles15.xml-p007744803p009518046.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles16.xml-p009518059p011539266.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles17.xml-p011539268p013693066.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles18.xml-p013693075p016120541.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles19.xml-p016120548p018754723.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles20.xml-p018754736p021222156.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles21.xml-p021222161p023927980.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles22.xml-p023927984p026823658.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles23.xml-p026823661p030503448.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles24.xml-p030503454p033952815.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles25.xml-p033952817p038067198.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles26.xml-p038067204p042663461.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+pv enwiki-20160204-pages-articles27.xml-p042663464p049318148.bz2 | lbzip2 -cd | java -Xmx512m -Xms128m -XX:NewSize=32m -XX:MaxNewSize=64m -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:GCTimeRatio=9 -XX:AdaptiveSizeDecrementScaleFactor=1 -server -jar /root/wp-download/mwdumper/mwdumper/target/mwdumper-1.25.jar --output=mysql://127.0.0.1/enwiki?user=website\&password=JJHex3pBjpsbTyVy  --format=sql:1.25
+
